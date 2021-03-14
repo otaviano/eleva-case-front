9d530e29c020122c8e792b68e84dc8c5bc7b72b7
Otaviano M. Zibetti <otaviano.zibetti_ext@stone.com.br>
2021-03-14 18:25:36 -0300

    adding unit tests to service

--- src/app/class/services/class.service.spec.ts
+++ src/app/class/services/class.service.spec.ts
@@ -1,14 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs/internal/observable/of';
 
+import { Class } from '../models';
 import { ClassService } from './class.service';
 
 describe('ClassService', () => {
   let service: ClassService;
+  let httpClientSpy: { post: jasmine.Spy, get: jasmine.Spy};
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ClassService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post', 'get']);
+    service = new ClassService(httpClientSpy as any);
   });
+
+  it('should be create a class (HttpClient called once)', () => {
+    const expectedClass: Class = { 
+      SchoolId: 1, 
+      Name: 'xpto', 
+      Description: 'xpto' 
+    };
+  
+    httpClientSpy.post.and.returnValue(of(expectedClass));
+  
+    service.create(expectedClass).subscribe(
+      c => expect(c).toEqual(expectedClass, 'expected class'),
+      fail
+    );
+    
+    expect(httpClientSpy.post.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return expected classes (HttpClient called once)', () => {
+    const expectedClasses: Class[] =
+      [
+        { SchoolId: 1, Name: 'xpto1', Description: 'xpto1' }, 
+        { SchoolId: 2, Name: 'xpto2', Description: 'xpto2' }
+      ];
+  
+    httpClientSpy.get.and.returnValue(of(expectedClasses));
+  
+    service.readAll().subscribe(
+      classes => expect(classes).toEqual(expectedClasses, 'expected classes'),
+      fail
+    );
+    
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
--- src/app/school/services/school.service.spec.ts
+++ src/app/school/services/school.service.spec.ts
@@ -1,13 +1,48 @@
-import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs/internal/observable/of';
+import { School } from '../models/school.model';
 
 import { SchoolService } from './school.service';
 
 describe('SchoolService', () => {
   let service: SchoolService;
+  let httpClientSpy: { post: jasmine.Spy, get: jasmine.Spy};
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SchoolService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post', 'get']);
+    service = new SchoolService(httpClientSpy as any);
+  });
+
+  it('should be create a School (HttpClient called once)', () => {
+    const expectedSchool: School = { 
+      Id: 1, 
+      Name: 'xpto', 
+      Description: 'xpto' 
+    };
+  
+    httpClientSpy.post.and.returnValue(of(expectedSchool));
+  
+    service.create(expectedSchool).subscribe(
+      c => expect(c).toEqual(expectedSchool, 'expected School'),
+      fail
+    );
+    
+    expect(httpClientSpy.post.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return expected schools (HttpClient called once)', () => {
+    const expectedSchools: School[] =
+      [
+        { Id: 1, Name: 'xpto1', Description: 'xpto1' }, 
+        { Id: 2, Name: 'xpto2', Description: 'xpto2' }
+      ];
+  
+    httpClientSpy.get.and.returnValue(of(expectedSchools));
+  
+    service.readAll().subscribe(
+      schools => expect(schools).toEqual(expectedSchools, 'expected schools'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 
   it('should be created', () => {
